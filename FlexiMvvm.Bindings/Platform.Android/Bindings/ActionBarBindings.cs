a9ee2119cad14f7d5e052c27839cf2378568ac58
Aliaksei Safonau <Aliaksei_Safonau@epam.com>
2018-11-29 10:55:50 +0300

    Add XML documentation to ActionBar bindings

--- FlexiMvvm.Bindings/Platform.Android/Bindings/ActionBarBindings.cs
+++ FlexiMvvm.Bindings/Platform.Android/Bindings/ActionBarBindings.cs
@@ -15,14 +15,22 @@
 // =========================================================================
 
 using System;
+using Android.Graphics.Drawables;
 using Android.Support.V7.App;
 using FlexiMvvm.Bindings.Custom;
+using Java.Lang;
 using JetBrains.Annotations;
 
 namespace FlexiMvvm.Bindings
 {
     public static class ActionBarBindings
     {
+        /// <summary>
+        /// Binding on <see cref="ActionBar.HideOffset"/> property.
+        /// </summary>
+        /// <param name="actionBarReference">The action bar reference.</param>
+        /// <returns>Binding</returns>
+        /// <exception cref="ArgumentNullException">actionBarReference</exception>
         [NotNull]
         public static TargetItemBinding<ActionBar, int> HideOffsetBinding(
             [NotNull] this IItemReference<ActionBar> actionBarReference)
@@ -36,6 +44,12 @@ namespace FlexiMvvm.Bindings
                 () => "HideOffset");
         }
 
+        /// <summary>
+        /// Binding on <see cref="ActionBar.HideOnContentScrollEnabled"/> property.
+        /// </summary>
+        /// <param name="actionBarReference">The action bar reference.</param>
+        /// <returns>Binding</returns>
+        /// <exception cref="ArgumentNullException">actionBarReference</exception>
         [NotNull]
         public static TargetItemBinding<ActionBar, bool> HideOnContentScrollEnabledBinding(
             [NotNull] this IItemReference<ActionBar> actionBarReference)
@@ -49,6 +63,12 @@ namespace FlexiMvvm.Bindings
                 () => "HideOnContentScrollEnabled");
         }
 
+        /// <summary>
+        /// Binding on <see cref="ActionBar.MenuVisibility"/> event.
+        /// </summary>
+        /// <param name="actionBarReference">The action bar reference.</param>
+        /// <returns>Binding</returns>
+        /// <exception cref="ArgumentNullException">actionBarReference</exception>
         [NotNull]
         public static TargetItemBinding<ActionBar, object> MenuVisibilityBinding(
             [NotNull] this IItemReference<ActionBar> actionBarReference)
@@ -65,6 +85,12 @@ namespace FlexiMvvm.Bindings
                 () => "MenuVisibility");
         }
 
+        /// <summary>
+        /// Binding on <see cref="ActionBar.Subtitle"/> property.
+        /// </summary>
+        /// <param name="actionBarReference">The action bar reference.</param>
+        /// <returns>Binding</returns>
+        /// <exception cref="ArgumentNullException">actionBarReference</exception>
         [NotNull]
         public static TargetItemBinding<ActionBar, string> SubtitleBinding(
             [NotNull] this IItemReference<ActionBar> actionBarReference)
@@ -78,6 +104,12 @@ namespace FlexiMvvm.Bindings
                 () => "Subtitle");
         }
 
+        /// <summary>
+        /// Binding on <see cref="ActionBar.Title"/> property.
+        /// </summary>
+        /// <param name="actionBarReference">The action bar reference.</param>
+        /// <returns>Binding</returns>
+        /// <exception cref="ArgumentNullException">actionBarReference</exception>
         [NotNull]
         public static TargetItemBinding<ActionBar, string> TitleBinding(
             [NotNull] this IItemReference<ActionBar> actionBarReference)
@@ -91,32 +123,95 @@ namespace FlexiMvvm.Bindings
                 () => "Title");
         }
 
+        /// <summary>
+        /// Binding on <see cref="ActionBar.SetHomeActionContentDescription()"/> method.
+        /// <para>
+        /// Supported types for <see cref="TValue"/>: <see cref="ICharSequence"/>, <see cref="string"/>, <see cref="int"/>
+        /// </para>
+        /// <para>
+        /// Usage: <c>.For(v => v.SetHomeActionContentDescriptionBinding&lt;ICharSequence&gt;())</c>
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="actionBarReference">The action bar reference.</param>
+        /// <returns>Binding</returns>
+        /// <exception cref="ArgumentNullException">actionBarReference</exception>
+        /// <exception cref="NotSupportedException">actionBarReference</exception>
         [NotNull]
-        public static TargetItemBinding<ActionBar, int> SetHomeActionContentDescriptionBinding(
+        public static TargetItemBinding<ActionBar, TValue> SetHomeActionContentDescriptionBinding<TValue>(
             [NotNull] this IItemReference<ActionBar> actionBarReference)
         {
             if (actionBarReference == null)
                 throw new ArgumentNullException(nameof(actionBarReference));
 
-            return new TargetItemOneWayCustomBinding<ActionBar, int>(
+            return new TargetItemOneWayCustomBinding<ActionBar, TValue>(
                 actionBarReference,
-                (actionBar, resId) => actionBar.NotNull().SetHomeActionContentDescription(resId),
+                (actionBar, value) =>
+                {
+                    switch (value)
+                    {
+                        case int resId:
+                            actionBar.NotNull().SetHomeActionContentDescription(resId);
+                            break;
+                        case ICharSequence charSequence:
+                            actionBar.NotNull().SetHomeActionContentDescription(charSequence);
+                            break;
+                        case string @string:
+                            actionBar.NotNull().SetHomeActionContentDescription(@string);
+                            break;
+                        default:
+                            throw new NotSupportedException($"{nameof(SetHomeActionContentDescriptionBinding)} doesn't support type {typeof(TValue)}");
+                    }
+                },
                 () => "SetHomeActionContentDescription");
         }
 
+        /// <summary>
+        /// Binding on <see cref="ActionBar.SetHomeAsUpIndicator()"/> method.
+        /// <para>
+        /// Supported types for <see cref="TValue"/>: <see cref="Drawable"/>, <see cref="int"/>
+        /// </para>
+        /// <para>
+        /// Usage: <c>.For(v => v.SetHomeActionContentDescriptionBinding&lt;Drawable&gt;())</c>
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="actionBarReference">The action bar reference.</param>
+        /// <returns>Binding</returns>
+        /// <exception cref="ArgumentNullException">actionBarReference</exception>
+        /// <exception cref="NotSupportedException">actionBarReference</exception>
         [NotNull]
-        public static TargetItemBinding<ActionBar, int> SetHomeAsUpIndicatorBinding(
+        public static TargetItemBinding<ActionBar, TValue> SetHomeAsUpIndicatorBinding<TValue>(
             [NotNull] this IItemReference<ActionBar> actionBarReference)
         {
             if (actionBarReference == null)
                 throw new ArgumentNullException(nameof(actionBarReference));
 
-            return new TargetItemOneWayCustomBinding<ActionBar, int>(
+            return new TargetItemOneWayCustomBinding<ActionBar, TValue>(
                 actionBarReference,
-                (actionBar, resId) => actionBar.NotNull().SetHomeAsUpIndicator(resId),
+                (actionBar, value) =>
+                {
+                    switch (value)
+                    {
+                        case int resId:
+                            actionBar.NotNull().SetHomeAsUpIndicator(resId);
+                            break;
+                        case Drawable drawable:
+                            actionBar.NotNull().SetHomeAsUpIndicator(drawable);
+                            break;
+                        default:
+                            throw new NotSupportedException($"{nameof(SetHomeAsUpIndicatorBinding)} doesn't support type {typeof(TValue)}");
+                    }
+                },
                 () => "SetHomeAsUpIndicator");
         }
 
+        /// <summary>
+        /// Binding on <see cref="ActionBar.SetHomeButtonEnabled()"/> method.
+        /// </summary>
+        /// <param name="actionBarReference">The action bar reference.</param>
+        /// <returns>Binding</returns>
+        /// <exception cref="ArgumentNullException">actionBarReference</exception>
         [NotNull]
         public static TargetItemBinding<ActionBar, bool> SetHomeButtonEnabledBinding(
             [NotNull] this IItemReference<ActionBar> actionBarReference)
---
+++ FlexiMvvm.Bindings/Platform.iOS/Bindings/UIStepperBindings.cs
@@ -0,0 +1,174 @@
+﻿// =========================================================================
+// Copyright 2018 EPAM Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =========================================================================
+
+using System;
+using FlexiMvvm.Bindings.Custom;
+using JetBrains.Annotations;
+using UIKit;
+
+namespace FlexiMvvm.Bindings
+{
+    public static class UIStepperBindings
+    {
+        [NotNull]
+        public static TargetItemBinding<UIStepper, bool> AutoRepeatBinding(
+            [NotNull] this IItemReference<UIStepper> stepperReference)
+        {
+            if (stepperReference == null)
+                throw new ArgumentNullException(nameof(stepperReference));
+
+            return new TargetItemOneWayCustomBinding<UIStepper, bool>(
+                stepperReference,
+                (stepper, autoRepeat) => stepper.NotNull().AutoRepeat = autoRepeat,
+                () => "AutoRepeat");
+        }
+
+        [NotNull]
+        public static TargetItemBinding<UIStepper, bool> ContinuousBinding(
+            [NotNull] this IItemReference<UIStepper> stepperReference)
+        {
+            if (stepperReference == null)
+                throw new ArgumentNullException(nameof(stepperReference));
+
+            return new TargetItemOneWayCustomBinding<UIStepper, bool>(
+                stepperReference,
+                (stepper, continuous) => stepper.NotNull().Continuous = continuous,
+                () => "Continuous");
+        }
+
+        [NotNull]
+        public static TargetItemBinding<UIStepper, double> MaximumValueBinding(
+            [NotNull] this IItemReference<UIStepper> stepperReference)
+        {
+            if (stepperReference == null)
+                throw new ArgumentNullException(nameof(stepperReference));
+
+            return new TargetItemOneWayCustomBinding<UIStepper, double>(
+                stepperReference,
+                (stepper, maximumValue) => stepper.NotNull().MaximumValue = maximumValue,
+                () => "MaximumValue");
+        }
+
+        [NotNull]
+        public static TargetItemBinding<UIStepper, double> MinimumValueBinding(
+            [NotNull] this IItemReference<UIStepper> stepperReference)
+        {
+            if (stepperReference == null)
+                throw new ArgumentNullException(nameof(stepperReference));
+
+            return new TargetItemOneWayCustomBinding<UIStepper, double>(
+                stepperReference,
+                (stepper, minimumValue) => stepper.NotNull().MinimumValue = minimumValue,
+                () => "MinimumValue");
+        }
+
+        [NotNull]
+        public static TargetItemBinding<UIStepper, UIImage> SetBackgroundImageBinding(
+            [NotNull] this IItemReference<UIStepper> stepperReference,
+            UIControlState controlState = UIControlState.Normal)
+        {
+            if (stepperReference == null)
+                throw new ArgumentNullException(nameof(stepperReference));
+
+            return new TargetItemOneWayCustomBinding<UIStepper, UIImage>(
+                stepperReference,
+                (stepper, image) => stepper.NotNull().SetBackgroundImage(image, controlState),
+                () => "SetBackgroundImage");
+        }
+
+        [NotNull]
+        public static TargetItemBinding<UIStepper, UIImage> SetDecrementImageBinding(
+            [NotNull] this IItemReference<UIStepper> stepperReference,
+            UIControlState controlState = UIControlState.Normal)
+        {
+            if (stepperReference == null)
+                throw new ArgumentNullException(nameof(stepperReference));
+
+            return new TargetItemOneWayCustomBinding<UIStepper, UIImage>(
+                stepperReference,
+                (stepper, image) => stepper.NotNull().SetDecrementImage(image, controlState),
+                () => "SetDecrementImage");
+        }
+
+        [NotNull]
+        public static TargetItemBinding<UIStepper, UIImage> SetDividerImageBinding(
+            [NotNull] this IItemReference<UIStepper> stepperReference,
+            UIControlState leftState = UIControlState.Normal,
+            UIControlState rightState = UIControlState.Normal)
+        {
+            if (stepperReference == null)
+                throw new ArgumentNullException(nameof(stepperReference));
+
+            return new TargetItemOneWayCustomBinding<UIStepper, UIImage>(
+                stepperReference,
+                (stepper, image) => stepper.NotNull().SetDividerImage(image, leftState, rightState),
+                () => "SetDividerImage");
+        }
+
+        [NotNull]
+        public static TargetItemBinding<UIStepper, UIImage> SetIncrementImageBinding(
+            [NotNull] this IItemReference<UIStepper> stepperReference,
+            UIControlState controlState = UIControlState.Normal)
+        {
+            if (stepperReference == null)
+                throw new ArgumentNullException(nameof(stepperReference));
+
+            return new TargetItemOneWayCustomBinding<UIStepper, UIImage>(
+                stepperReference,
+                (stepper, image) => stepper.NotNull().SetIncrementImage(image, controlState),
+                () => "SetIncrementImage");
+        }
+
+        [NotNull]
+        public static TargetItemBinding<UIStepper, double> StepValueBinding(
+            [NotNull] this IItemReference<UIStepper> stepperReference)
+        {
+            if (stepperReference == null)
+                throw new ArgumentNullException(nameof(stepperReference));
+
+            return new TargetItemOneWayCustomBinding<UIStepper, double>(
+                stepperReference,
+                (stepper, stepValue) => stepper.NotNull().StepValue = stepValue,
+                () => "StepValue");
+        }
+
+        [NotNull]
+        public static TargetItemBinding<UIStepper, double> ValueBinding(
+            [NotNull] this IItemReference<UIStepper> stepperReference)
+        {
+            if (stepperReference == null)
+                throw new ArgumentNullException(nameof(stepperReference));
+
+            return new TargetItemOneWayCustomBinding<UIStepper, double>(
+                stepperReference,
+                (stepper, value) => stepper.NotNull().Value = value,
+                () => "Value");
+        }
+
+        [NotNull]
+        public static TargetItemBinding<UIStepper, bool> WrapsBinding(
+            [NotNull] this IItemReference<UIStepper> stepperReference)
+        {
+            if (stepperReference == null)
+                throw new ArgumentNullException(nameof(stepperReference));
+
+            return new TargetItemOneWayCustomBinding<UIStepper, bool>(
+                stepperReference,
+                (stepper, wraps) => stepper.NotNull().Wraps = wraps,
+                () => "Wraps");
+        }
+    }
+}
