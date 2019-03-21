a8a0d5677a1efeb598a03d22bc55c6817cc00dfc
Yauhen Papou <yauhen.papou@gmail.com>
2019-03-21 10:40:41 +0300

    Sync Android navigation with iOS one.

---
+++ FlexiMvvm.Lifecycle/Platform.Android/Navigation/NavigationViewProvider.cs
@@ -0,0 +1,66 @@
+﻿// =========================================================================
+// Copyright 2019 EPAM Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =========================================================================
+
+using System;
+using FlexiMvvm.ViewModels;
+using FlexiMvvm.Views;
+using FlexiMvvm.Views.Core;
+using Fragment = Android.Support.V4.App.Fragment;
+using FragmentActivity = Android.Support.V4.App.FragmentActivity;
+
+namespace FlexiMvvm.Navigation
+{
+    public static partial class NavigationViewProvider
+    {
+        /// <summary>
+        /// Gets the navigation view derived from the <see cref="FragmentActivity"/>.
+        /// </summary>
+        /// <typeparam name="TView">The type of the view.</typeparam>
+        /// <typeparam name="TViewModel">The type of the view model.</typeparam>
+        /// <param name="viewModel">The view model used for getting a bound view.</param>
+        /// <returns>The navigation view.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="viewModel"/> is <c>null</c>.</exception>
+        /// <exception cref="InvalidOperationException">The view instance is missing for <paramref name="viewModel"/> instance.</exception>
+        public static TView GetActivity<TView, TViewModel>(TViewModel viewModel)
+            where TView : FragmentActivity, INavigationView<TViewModel>
+            where TViewModel : class, IViewModel
+        {
+            if (viewModel == null)
+                throw new ArgumentNullException(nameof(viewModel));
+
+            return ViewCache.Get<TView, TViewModel>(viewModel);
+        }
+
+        /// <summary>
+        /// Gets the navigation view derived from the <see cref="Fragment"/>.
+        /// </summary>
+        /// <typeparam name="TView">The type of the view.</typeparam>
+        /// <typeparam name="TViewModel">The type of the view model.</typeparam>
+        /// <param name="viewModel">The view model used for getting a bound view.</param>
+        /// <returns>The navigation view.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="viewModel"/> is <c>null</c>.</exception>
+        /// <exception cref="InvalidOperationException">The view instance is missing for <paramref name="viewModel"/> instance.</exception>
+        public static TView GetFragment<TView, TViewModel>(TViewModel viewModel)
+            where TView : Fragment, INavigationView<TViewModel>
+            where TViewModel : class, IViewModel
+        {
+            if (viewModel == null)
+                throw new ArgumentNullException(nameof(viewModel));
+
+            return ViewCache.Get<TView, TViewModel>(viewModel);
+        }
+    }
+}
---
+++ FlexiMvvm.Lifecycle/Platform.Android/Views/!Extensions/ViewExtensions.cs
@@ -0,0 +1,110 @@
+﻿// =========================================================================
+// Copyright 2019 EPAM Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =========================================================================
+
+using System;
+using FlexiMvvm.Formatters;
+using FlexiMvvm.ViewModels;
+
+namespace FlexiMvvm.Views
+{
+    /// <summary>
+    /// Provides a set of static methods for accessing the <see cref="IView{TViewModel}"/>.
+    /// </summary>
+    public static class ViewExtensions
+    {
+        /// <summary>
+        /// Executes appropriate handler based on actual type of the <paramref name="view"/>.
+        /// </summary>
+        /// <param name="view">The view that is represented by an activity or fragment.</param>
+        /// <param name="activityHandler">The handler to execute if the <paramref name="view"/> is <see cref="Android.Support.V4.App.FragmentActivity"/>.</param>
+        /// <param name="fragmentHandler">The handler to execute if the <paramref name="view"/> is <see cref="Android.Support.V4.App.Fragment"/>.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="view" /> or <paramref name="activityHandler" /> or <paramref name="fragmentHandler" /> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="view" /> is derived from a class other than the <see cref="Android.Support.V4.App.FragmentActivity"/> or <see cref="Android.Support.V4.App.Fragment"/>.
+        /// </exception>
+        public static void As(
+            this IView<IViewModel> view,
+            Action<Android.Support.V4.App.FragmentActivity> activityHandler,
+            Action<Android.Support.V4.App.Fragment> fragmentHandler)
+        {
+            if (view == null)
+                throw new ArgumentNullException(nameof(view));
+            if (activityHandler == null)
+                throw new ArgumentNullException(nameof(activityHandler));
+            if (fragmentHandler == null)
+                throw new ArgumentNullException(nameof(fragmentHandler));
+
+            if (view is Android.Support.V4.App.FragmentActivity activity)
+            {
+                activityHandler(activity);
+            }
+            else if (view is Android.Support.V4.App.Fragment fragment)
+            {
+                fragmentHandler(fragment);
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"Only views derived from '{TypeFormatter.FormatName<Android.Support.V4.App.FragmentActivity>()}' or " +
+                    $"'{TypeFormatter.FormatName<Android.Support.V4.App.Fragment>()}' are supported.", nameof(view));
+            }
+        }
+
+        /// <summary>
+        /// Executes appropriate handler based on actual type of the <paramref name="view"/> with returning a result.
+        /// </summary>
+        /// <typeparam name="T">The type of the result.</typeparam>
+        /// <param name="view">The view that is represented by an activity or fragment.</param>
+        /// <param name="activityHandler">The handler to execute if the <paramref name="view"/> is <see cref="Android.Support.V4.App.FragmentActivity"/>.</param>
+        /// <param name="fragmentHandler">The handler to execute if the <paramref name="view"/> is <see cref="Android.Support.V4.App.Fragment"/>.</param>
+        /// <returns>A result returned by appropriate handler.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="view" /> or <paramref name="activityHandler" /> or <paramref name="fragmentHandler" /> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="view" /> is derived from a class other than the <see cref="Android.Support.V4.App.FragmentActivity"/> or <see cref="Android.Support.V4.App.Fragment"/>.
+        /// </exception>
+        public static T As<T>(
+            this IView<IViewModel> view,
+            Func<Android.Support.V4.App.FragmentActivity, T> activityHandler,
+            Func<Android.Support.V4.App.Fragment, T> fragmentHandler)
+        {
+            if (view == null)
+                throw new ArgumentNullException(nameof(view));
+            if (activityHandler == null)
+                throw new ArgumentNullException(nameof(activityHandler));
+            if (fragmentHandler == null)
+                throw new ArgumentNullException(nameof(fragmentHandler));
+
+            T result;
+
+            if (view is Android.Support.V4.App.FragmentActivity activity)
+            {
+                result = activityHandler(activity);
+            }
+            else if (view is Android.Support.V4.App.Fragment fragment)
+            {
+                result = fragmentHandler(fragment);
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"Only views derived from '{TypeFormatter.FormatName<Android.Support.V4.App.FragmentActivity>()}' or " +
+                    $"'{TypeFormatter.FormatName<Android.Support.V4.App.Fragment>()}' are supported.", nameof(view));
+            }
+
+            return result;
+        }
+    }
+}
