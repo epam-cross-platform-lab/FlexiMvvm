a36be7acc8da6378178b85d48dc34f8fa99bca66
Yauhen Papou <yauhen.papou@gmail.com>
2019-04-12 14:25:27 +0300

    Don't throw an exception if a parameter passed to a Command that doesn't expect any parameter.

--- FlexiMvvm.Common/Commands/Command.cs
+++ FlexiMvvm.Common/Commands/Command.cs
@@ -35,13 +35,6 @@ namespace FlexiMvvm.Commands
 
         bool ICommand.CanExecute(object parameter)
         {
-            if (parameter != null)
-            {
-                throw new ArgumentException(
-                    $"\"{_name.SelfOrDefaultIfNullOrWhiteSpace(TypeFormatter.FormatName(GetType()))}\" command expects \"null\" value.",
-                    nameof(parameter));
-            }
-
             return CanExecute();
         }
 
@@ -49,13 +42,6 @@ namespace FlexiMvvm.Commands
 
         void ICommand.Execute(object parameter)
         {
-            if (parameter != null)
-            {
-                throw new ArgumentException(
-                    $"\"{_name.SelfOrDefaultIfNullOrWhiteSpace(TypeFormatter.FormatName(GetType()))}\" command expects \"null\" value.",
-                    nameof(parameter));
-            }
-
             Execute();
         }
 
