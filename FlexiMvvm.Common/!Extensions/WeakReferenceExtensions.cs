7a5d2404fab6a1fc472061e3f5e47452f30f2138
Yauhen Papou <yauhen_papou@epam.com>
2020-10-18 19:51:14 +0300

    Return back WeakReferenceExtensions.

---
+++ FlexiMvvm.Common/!Extensions/WeakReferenceExtensions.cs
@@ -0,0 +1,32 @@
+﻿// =========================================================================
+// Copyright 2019 EPAM Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =========================================================================
+
+using System;
+
+namespace FlexiMvvm
+{
+    public static class WeakReferenceExtensions
+    {
+        public static T? GetTarget<T>(this WeakReference<T> weakReference)
+            where T : class
+        {
+            if (weakReference == null)
+                throw new ArgumentNullException(nameof(weakReference));
+
+            return weakReference.TryGetTarget(out var target) ? target : default;
+        }
+    }
+}
